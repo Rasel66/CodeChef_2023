3f9470f02d3ee919c9dea2f65f4e25abb67ca855
Md. Rasel Hossain <raselhossain5437@gmail.com>
2023-09-10 20:58:23 +0600

    Equal elements

---
+++ Equal Elements.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int arr[n];
+        vector<int> count(n+1, 0);
+        for(int i =0;i<n;i++){
+            cin>> arr[i];
+            count[arr[i]]++;
+        }
+        sort(count.begin(), count.end());
+
+        int max = count[n];
+
+        cout<< n-max << endl;
+    }
+}
